b0317a2639abcbb1c255e99b074732004b9aa3b4
Al Tsaplin <al.tsaplin@gmail.com>
2023-09-06 12:30:37 +0300

    feat(slider images) Finished.

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,22 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
+'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
+import Swiper from 'swiper';
 
-	// Custom JS
+import { Keyboard, Mousewheel, Parallax } from 'swiper/modules';
 
-})
+const swiperImages = new Swiper('.slider-images', {
+  modules: [Keyboard, Mousewheel, Parallax],
+  loop: false,
+  speed: 2400,
+  parallax: true,
+  mousewheel: {
+    invert: false,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+});
+
+// Just in case, later to delete.
+// document.addEventListener('DOMContentLoaded', () => {});
