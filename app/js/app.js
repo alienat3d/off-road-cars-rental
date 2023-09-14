7165ec64200a493d43667ef5e05559be4efcf291
Al Tsaplin <al.tsaplin@gmail.com>
2023-09-15 00:50:25 +0300

    [WIT] Add gear decor element. Animated it with GASP. Also added a slides counter to upper right corner. Added a current slide number to bottom right corner.

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,17 @@
 
 import Swiper from 'swiper';
 
-import { Keyboard, Mousewheel, Parallax, Controller, Pagination, Scrollbar, Navigation } from 'swiper/modules';
+import {
+  Keyboard,
+  Mousewheel,
+  Parallax,
+  Controller,
+  Pagination,
+  Scrollbar,
+  Navigation,
+} from 'swiper/modules';
+
+import { gsap, Power2 } from 'gsap';
 
 const swiperImages = new Swiper('.slider-images', {
   modules: [Parallax, Controller],
@@ -12,7 +22,14 @@ const swiperImages = new Swiper('.slider-images', {
 });
 
 const swiperText = new Swiper('.slider-text', {
-  modules: [Keyboard, Mousewheel, Controller, Pagination, Scrollbar, Navigation],
+  modules: [
+    Keyboard,
+    Mousewheel,
+    Controller,
+    Pagination,
+    Scrollbar,
+    Navigation,
+  ],
   loop: false,
   speed: 2400,
   mousewheel: {
@@ -36,8 +53,24 @@ const swiperText = new Swiper('.slider-text', {
   },
 });
 
+const gear = document.querySelector('.slider-gear')
+
 swiperImages.controller.control = swiperText;
 swiperText.controller.control = swiperImages;
+
+swiperText.on('slideNextTransitionStart', () => {
+  gsap.to(gear, 2.8, {
+    rotation: '+=50',
+    ease: Power2.easeOut
+  })
+})
+
+swiperText.on('slidePrevTransitionStart', () => {
+  gsap.to(gear, 2.8, {
+    rotation: '-=50',
+    ease: Power2.easeOut
+  })
+})
 
 // Just in case, later to delete.
 // document.addEventListener('DOMContentLoaded', () => {});
